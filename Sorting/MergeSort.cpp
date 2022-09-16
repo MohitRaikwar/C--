5d44ff18738e5e70b892c4b675a67084f9d08f0a
Mohit Kumar <vegasgangstar25@gmail.com>
2022-09-16 09:21:28 +0530

    Added Function Runtime calc.

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <chrono>
+#include <stdlib.h>
 using namespace std;
 
 // functions prototype
-void print_array(int arr[],int n);
-void input_array(int arr[], int n);
+void print_array(int arr[], int n);
+void generate_array(int arr[], int n);
 void merge(int arr[], int left, int mid, int right);
-void mergeSort(int arr[],int left,int right);
+void mergeSort(int arr[], int left, int right);
 
 int main()
 {
@@ -13,29 +15,29 @@ int main()
     cout << "Enter size of array :";
     cin >> size;
     int arr[size];
-    input_array(arr, size);
-    cout<<"\nBefore sorting : ";
-    print_array(arr,size);
-    mergeSort(arr,0,size-1);
-    cout<<"\nAfter sorting :";
-    print_array(arr,size);
+    cout << "\nBefore sorting : ";
+    print_array(arr, size);
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort(arr, 0, size - 1);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration =std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    cout << "\nAfter sorting :";
+    print_array(arr, size);
+    cout << "Time taken by Merge Sort function: " << duration.count() << " microseconds\n";
     return 0;
 }
 
-void print_array(int arr[],int n)
-{
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
-    cout<<"\n";
-}
-
-void input_array(int arr[], int n)
+void print_array(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
-    {
-        cout << "Enter " << i + 1 << " element :";
-        cin >> arr[i];
-    }
+        cout << "arr[" << i << "] : " << arr[i] << "\n";
+    cout << "\n";
+}
+
+void generate_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        arr[i] = rand();
 }
 
 void merge(int arr[], int left, int mid, int right)
@@ -59,52 +61,48 @@ void merge(int arr[], int left, int mid, int right)
     int indexOfSubarrayTwo{0};
     int indexOfMergedArray{left};
 
-    //comparing elements of both subarray
+    // comparing elements of both subarray
     while (indexOfSubarrayOne < size_subarray_one && indexOfSubarrayTwo < size_subarray_two)
     {
-        if(leftArray[indexOfSubarrayOne]<=rightArray[indexOfSubarrayTwo])
+        if (leftArray[indexOfSubarrayOne] <= rightArray[indexOfSubarrayTwo])
         {
             arr[indexOfMergedArray] = leftArray[indexOfSubarrayOne];
             indexOfSubarrayOne++;
         }
         else
         {
-            arr[indexOfMergedArray]=rightArray[indexOfSubarrayTwo];
+            arr[indexOfMergedArray] = rightArray[indexOfSubarrayTwo];
             indexOfSubarrayTwo++;
         }
         indexOfMergedArray++;
-
     }
 
-    //copy remaining items of subarrayone
-    while(indexOfSubarrayOne<size_subarray_one)
+    // copy remaining items of subarrayone
+    while (indexOfSubarrayOne < size_subarray_one)
     {
-        arr[indexOfMergedArray]=leftArray[indexOfSubarrayOne];
+        arr[indexOfMergedArray] = leftArray[indexOfSubarrayOne];
         indexOfMergedArray++;
         indexOfSubarrayOne++;
-
     }
 
-    //copy remaining items of subarraytwo
-    while(indexOfSubarrayTwo<size_subarray_two)
+    // copy remaining items of subarraytwo
+    while (indexOfSubarrayTwo < size_subarray_two)
     {
-        arr[indexOfMergedArray]=rightArray[indexOfSubarrayTwo];
+        arr[indexOfMergedArray] = rightArray[indexOfSubarrayTwo];
         indexOfMergedArray++;
         indexOfSubarrayTwo++;
-
     }
 }
 
-void mergeSort(int arr[],int left,int right)
+void mergeSort(int arr[], int left, int right)
 {
-    if(left>=right)
+    if (left >= right)
         return;
     else
-    {   
-        int mid=left+(right-left)/2;
-        mergeSort(arr,left,mid);
-        mergeSort(arr,mid+1,right);
-        merge(arr,left,mid,right);
-
+    {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
