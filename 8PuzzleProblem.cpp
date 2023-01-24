283277fa40176f55286fda9c06370894acf15a51
Proton Kashyap <vegasgangstar25@gmail.com>
2023-01-25 00:10:58 +0530

    8PuzzleProblem solution using bfs and dfs

--- 8PuzzleProblem.cpp
+++ 8PuzzleProblem.cpp
@@ -14,18 +14,76 @@ struct Node
 
     // stores blank tile coordinates
     int x, y;
-
-    // checks if node is visited
-    bool visited = false;
 };
 
+// function prototypes
+
+void printMatrix(int mat[N][N]);
+int isSafe(int x, int y);
+Node *newNode(int mat[N][N], int x, int y, int newX, int newY, Node *parent);
+bool compareMatrix(int initial[N][N], int final[N][N]);
+void printPath(Node *root);
+bool checkIfParentExist(Node *node);
+void bfs(int initial[N][N], int x, int y, int final[N][N]);
+void dfs(Node *parent, int final[N][N]);
+
+// Main function
+int main()
+{
+    // Initial configuration
+    // Value 0 is used for empty space
+    int initial[N][N] =
+        {
+            {1, 2, 3},
+            {5, 6, 0},
+            {7, 8, 4}};
+
+    // Solvable Final configuration
+    // Value 0 is used for empty space
+    int final[N][N] =
+        {
+            {1, 2, 3},
+            {5, 8, 6},
+            {0, 7, 4}};
+
+    // Blank tile coordinates in initial
+    // configuration
+    int x = 1, y = 2;
+    if (compareMatrix(initial, final))
+    {
+        cout << "Initial matrix is the final matrix";
+        return 0;
+    }
+
+    int choice;
+    cout << "Solve 8-Puzzle Problem by \n";
+    cout << "1.Using BFS\n";
+    cout << "2.Using DFS\n";
+    cout << "Enter your choice :";
+    cin >> choice;
+    if (choice == 1)
+        bfs(initial, x, y, final);
+    else if (choice == 2)
+    {
+        Node *root = newNode(initial, x, y, x, y, NULL);
+        dfs(root, final);
+    }
+    else
+        cout << "\nWrong choice\n";
+
+    return 0;
+}
+
+// stores parent matrices
+vector<Node *> v;
+
 // function to print the matrix
-int printMatrix(int mat[N][N])
+void printMatrix(int mat[N][N])
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
-            cout << mat[N][N] << " ";
+            cout << mat[i][j] << " ";
         cout << "\n";
     }
 }
@@ -36,7 +94,7 @@ Node *newNode(int mat[N][N], int x, int y, int newX, int newY, Node *parent)
     Node *node = new Node;
     node->parent = parent;
 
-    // copy data from parent node to current node
+    // copy matrix
     memcpy(node->mat, mat, sizeof node->mat);
 
     // move tile by one position
@@ -45,9 +103,10 @@ Node *newNode(int mat[N][N], int x, int y, int newX, int newY, Node *parent)
     node->y = newY;
     return node;
 }
-// right,bottom,top,left
+
+// left,bottom,top,left
 int row[] = {0, 1, -1, 0};
-int col[] = {1, 0, 0, -1};
+int col[] = {-1, 0, 0, 1};
 
 // Function to check whether (x,y) is a valid matrix coordinate
 int isSafe(int x, int y)
@@ -55,8 +114,7 @@ int isSafe(int x, int y)
     return (x >= 0 && x < N && y >= 0 && y < N);
 }
 
-queue<Node *> matrixQueue;
-
+// Function to check if two matrices are equivalent
 bool compareMatrix(int initial[N][N], int final[N][N])
 {
     for (int i = 0; i < N; i++)
@@ -64,46 +122,98 @@ bool compareMatrix(int initial[N][N], int final[N][N])
         for (int j = 0; j < N; j++)
             if (initial[i][j] != final[i][j])
                 return false;
-        return true;
     }
+    return true;
 }
 
+// Function to print path from goal state to parent state
 void printPath(Node *root)
 {
     if (root->parent == NULL)
+    {
+        printMatrix(root->mat);
+        cout << endl;
         return;
+    }
     printPath(root->parent);
     printMatrix(root->mat);
+    cout << endl;
 }
 
-void solve(int initial[N][N], int x, int y, int final[N][N])
+bool checkIfParentExist(Node *node)
 {
-    Node *root = newNode(initial, x, y, x, y, NULL);
-    matrixQueue.push(root);
-    if (root->visited)
-        printPath(root);
-    else
-        dfs(root, final);
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (compareMatrix(node->mat, v[i]->mat))
+            return true;
+    }
+    return false;
 }
 
+// DFS solution to 8-Puzzle Problem
 void dfs(Node *parent, int final[N][N])
 {
-    parent->visited = true;
     for (int i = 0; i < 4; i++)
     {
         if (isSafe(parent->x + row[i], parent->y + col[i]))
         {
             Node *child = newNode(parent->mat, parent->x, parent->y, parent->x + row[i], parent->y + col[i], parent);
-            matrixQueue.push(child);
             if (compareMatrix(child->mat, final))
+            {
+                cout << "Path found";
                 printPath(child);
-            else
+                return;
+            }
+            else if (!checkIfParentExist(child))
+            {
+                if (i == 0)
+                    cout << "Left Movement\n";
+                else if (i == 1)
+                    cout << "Bottom Movement\n";
+                else if (i == 2)
+                    cout << "Top Movement\n";
+                else
+                    cout << "Right Movement\n";
+                v.push_back(child);
                 dfs(child, final);
+            }
         }
     }
 }
 
-int main()
+// BFS solution to the 8-Puzzle Problem
+void bfs(int initial[N][N], int x, int y, int final[N][N])
 {
-    return 0;
+    // queue to store child nodes
+    queue<Node *> q;
+    Node *root = newNode(initial, x, y, x, y, NULL);
+
+    // pushing the initial board configuration
+    q.push(root);
+    while (!q.empty())
+    {
+        // getting the front element of the queue
+        Node *node = q.front();
+
+        // popping out the first element
+        q.pop();
+
+        // this loop generates child nodes and pushes them back into queue
+        for (int i = 0; i < 4; i++)
+        {
+            if (isSafe(node->x + row[i], node->y + col[i]))
+            {
+                Node *child = newNode(node->mat, node->x, node->y, node->x + row[i], node->y + col[i], node);
+                if (compareMatrix(child->mat, final))
+                {
+                    cout << "Path found\n";
+                    printPath(child);
+                    return;
+                }
+                q.push(child);
+            }
+        }
+    }
+    cout << "Can't reach goal state \n";
+    return;
 }
